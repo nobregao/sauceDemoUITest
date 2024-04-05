3362282d65fec1fbb04ea7c7eab8649f2ebf54d8
renatão <paulo05renato@gmail.com>
2024-04-05 20:24:50 -0300

    Update sauceLogin.cy.js

--- cypress/e2e/sauceLogin.cy.js
+++ cypress/e2e/sauceLogin.cy.js
@@ -9,7 +9,7 @@ describe('Login no Sauce Labs', () => {
 
   it('verificar componentes da lista de produtos', () => {
     cy.get('.title').should('have.text', 'Products');
-    cy.get('[data-test="product_sort_container"]').should('be.visible');
+    cy.get('[data-test="product-sort-container"]').should('be.visible');
     cy.get('.shopping_cart_link').should('be.visible');
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').should('be.visible');
   })
